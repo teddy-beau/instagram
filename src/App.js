b1e3223467c14a30c0eba7bbc3bc6d32a4346887
teddy-beau <hello@teddy-beau.com>
2021-05-10 15:42:32 +0200

    dashboard completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import useAuthListener from "./hooks/use-auth-listener";
 import UserContext from "./context/user";
+import ProtectedRoute from "./helpers/protected-routes";
+import IsUserLoggedIn from "./helpers/is-user-logged-in";
 
 const Login = lazy(() => import("./pages/Login"));
 const SignUp = lazy(() => import("./pages/SignUp"));
 const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Profile = lazy(() => import("./pages/Profile"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
@@ -17,10 +20,24 @@ function App() {
          <Router>
             <Suspense fallback={<p>Loading...</p>}>
                <Switch>
-                  <Route path={ROUTES.LOGIN} component={Login} />
-                  <Route path={ROUTES.SIGN_UP} component={SignUp} />
-                  <Route path={ROUTES.DASHBOARD} component={Dashboard} />
-
+                  <IsUserLoggedIn
+                     user={user}
+                     loggedInPath={ROUTES.DASHBOARD}
+                     path={ROUTES.LOGIN}
+                  >
+                     <Login />
+                  </IsUserLoggedIn>
+                  <IsUserLoggedIn
+                     user={user}
+                     loggedInPath={ROUTES.DASHBOARD}
+                     path={ROUTES.SIGN_UP}
+                  >
+                     <SignUp />
+                  </IsUserLoggedIn>
+                  <Route path={ROUTES.PROFILE} component={Profile} />
+                  <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+                     <Dashboard />
+                  </ProtectedRoute>
                   <Route component={NotFound} />
                </Switch>
             </Suspense>
---
+++ src/pages/Profile.js
@@ -0,0 +1,4 @@
+const Profile = () => {
+   return <div className="container">This is the Profile component.</div>;
+};
+export default Profile;
